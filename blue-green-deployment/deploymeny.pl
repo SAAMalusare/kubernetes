a2f164962aacf6d5e1e45e0554bc18edbca84486
suhas.malusare@gmail.com <suhasx.suresh.malusare@intel.com>
2019-11-20 18:28:25 +0530

    Automate BG Deployment

--- blue-green-deployment/deploymeny.pl
+++ blue-green-deployment/deploymeny.pl
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use Kubernetes::REST;
+use Net::Kubernetes;
 use Data::Dumper;
 
 my  $ver = '1.0';
@@ -14,8 +14,8 @@ print "Blue-Green Deployment script, version: $ver\n";
 print "Deploying application version = $version \n";
 print "============================================\n";
 
-my  $kubectl = Kubernetes::REST->new();
-my  $result = $kubectl->Core->ListNamespacedPod();
-    print Dumper($result);
-print "Getting currently deployed version \n";
+my $kube = Net::Kubernetes->new();
+my $pod_list = $kube->list_pods();
+
+print  $pod_list; 
 
