ef5b8e83d329ee73b4ff0954b1590dbdd4563a0a
suhas.malusare@gmail.com <suhasx.suresh.malusare@intel.com>
2019-11-21 12:08:58 +0530

    Automate BG Deployment

--- blue-green-deployment/deploymeny.pl
+++ blue-green-deployment/deploymeny.pl
@@ -14,7 +14,7 @@ print "Blue-Green Deployment script, version: $ver\n";
 print "Deploying application version = $ver \n";
 print "============================================\n";
 
-my $kube = Net::Kubernetes->new(url=>'http://master:8080');
+my $kube = Net::Kubernetes->new(url=>'https://172.17.0.76:6443/');
 my $pod_list = $kube->list_pods();
 
 print  $pod_list; 
