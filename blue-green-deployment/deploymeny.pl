3ff32156452c0db3a021df94c24dabb844a43e0f
suhas.malusare@gmail.com <suhasx.suresh.malusare@intel.com>
2019-11-21 11:26:56 +0530

    Automate BG Deployment

--- blue-green-deployment/deploymeny.pl
+++ blue-green-deployment/deploymeny.pl
@@ -8,14 +8,13 @@ use Net::Kubernetes;
 my  $ver = '1.0';
 my  ($deployment, $version, $service);
 
-
-chomp ($deployment, $version, $service);
+#chomp ($deployment, $version, $service);
 print "============================================\n";
 print "Blue-Green Deployment script, version: $ver\n";
 print "Deploying application version = $ver \n";
 print "============================================\n";
 
-my $kube = Net::Kubernetes->new();
+my $kube = Net::Kubernetes->new(url=>'http://master:8080');
 my $pod_list = $kube->list_pods();
 
 print  $pod_list; 
